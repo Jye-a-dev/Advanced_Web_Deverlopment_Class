73d4335767b181748a6bc2a42c728447c3f6c842
Jye <shokoste1@gmail.com>
2026-04-06 12:46:12 +0700

    lab 10

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const disableCSP = require("./utils/disableCSP");
+const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,4 +21,27 @@ app.get("/hello", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
+});
+
+app.use(
+  '/admin',
+  express.static(path.resolve(__dirname, '../client-admin/build'))
+);
+
+app.get('/admin/*', (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, '../client-admin/build', 'index.html')
+  );
+});
+
+// '/' serve the files at client-customer/build/* as static files
+app.use(
+  '/',
+  express.static(path.resolve(__dirname, '../client-customer/build'))
+);
+
+app.get('*', (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, '../client-customer/build', 'index.html')
+  );
 });
